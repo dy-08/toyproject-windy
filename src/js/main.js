0c8829ece8e68dea791e79aad23a121b201199d8
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2025-09-18 02:54:59 +0900

    feat: SPA 구조 수정, weather-now API 연동 및 반응형 레이아웃 적용
    - SPA 구조에서 partial 로드 방식 개선
    - weather-now 페이지에 API 데이터 연동
    - weather-now 레이아웃 반응형 스타일링 추가

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,25 @@ let content = document.getElementById('content');
 let link = document.querySelectorAll('#pages a');
 link.forEach((item) => {
   item.addEventListener('click', () => {
-    let page = `/src/html/${item.getAttribute('data-page')}`;
-    fetch(page)
-      .then((response) => response.text())
-      .then((data) => (content.innerHTML = data))
+    let pagePath = `/src/html/${item.getAttribute('data-page')}`;
+    let pageName = item.getAttribute('data-page'); // weather-now.html
+    fetch(pagePath)
+      .then((res) => res.text())
+      .then((data) => {
+        content.innerHTML = data;
+
+        // 기존 동적 스크립트 제거
+        document
+          .querySelectorAll('script[data-dynamic]')
+          .forEach((s) => s.remove());
+        // 페이지별 JS 실행
+        if (pageName === 'weather-now.html') {
+          const script = document.createElement('script');
+          script.src = './src/js/weather-now.js';
+          script.setAttribute('data-dynamic', 'true');
+          document.body.appendChild(script);
+        }
+      })
       .catch((e) => console.log(e));
   });
 });
--- src/js/weather-now.js
+++ src/js/weather-now.js
@@ -1,53 +1,123 @@
 /**
- * T1H	기온
- * RN1	1시간 강수량
- * UUU	동서바람성분
- * VVV	남북바람성분
- * REH	습도
- * PTY	강수형태
- * VEC	풍향
- * WSD	풍속
+ * POP 강수확률 % ✅
+ * PTY 강수형태 코드값 ✅
+ * PCP 1시간강수량 범주(1mm) ✅
+ * REH 습도 % ✅
+ * SNO 1시간신적설 범주(1cm) ✅
+ * SKY 하늘상태 코드값 ✅
+ * TMP 1시간기온 °C ✅
+ * TMN 일최저기온 °C
+ * TMX 일최고기온 °C
+ * UUU 풍속(동서성분) m/s ✅
+ * VVV 풍속(남북성분) m/s ✅
+ * WAV 파고 M ✅
+ * VEC 풍향 deg ✅
+ * WSD 풍속 m/s ✅
+ 
+ * Pick: TMP(1시간기온), SKY(하늘상태), POP(강수확률), REH(습도), WSD(풍속)
+ * +a: 위치, 시간
+
+ * 코드값
+ * 하늘상태(SKY) 코드값: 맑음(1), 구름많음(3), 흐림(4)
+ * 강수형태(PTY) 코드값: 없음(0), 비(1), 비/눈(2), 눈(3), 소나기(4)
+ * 강수량 범주(PCP):  -, null, 0값은 (강수없음) 나머지는 18페이지 참조
+ * SNO 1시간신적설 범주(SNO): -, null, 0값은 (적설없음)
+ *
+
+ * TMN/TMX(일최저/최고) 는 하루 1회씩만 포함
+ * TMN은 새벽(02:00/05:00 차수) 예보에서만 내려옴
+ * TMX는 낮(11:00/14:00 차수) 예보에서만 내려옴
  */
 
-const category = document.querySelectorAll('.category');
-const obsrValue = document.querySelectorAll('.obsrValue');
-const tbody = document.getElementById('tbody');
-const parsedData = {};
-const weatherNow = document.getElementById('weather-now');
+function formatDate(date) {
+  const yyyy = date.getFullYear();
+  const mm = String(date.getMonth() + 1).padStart(2, '0'); // 9
+  const dd = String(date.getDate()).padStart(2, '0');
+  return `${yyyy}${mm}${dd}`;
+}
 
 try {
-    fetch(
-        'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=9424b102fa31a2cdf077b43d93ffbbd26e040beb866f241236f5a5e7913ab3ea&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20250916&base_time=0610&nx=57&ny=121'
-    )
-        .then((res) => res.json())
-        .then((data) => {
-            const datas = data.response.body.items.item;
-            console.log(datas);
-            // fetch의 url로 데이터를 받아올 수 있음 ✨
-            // 내가 원하는 데이터: 초단기실황 (기온, 1시간 강수량, 동서바람성분, 남북바람성분, 습도, 강수형태, 풍향, 풍속)
-            // 공공데이터포털에서의 초단기실황 날씨 데이터는 6시 초기화, 시간당 8개타입으로 분류 됨
-            // category 분류
-            // obsrValue 해당값
-            // 테이블로 뿌려주고 시작
-            datas.forEach((item) => {
-                parsedData[item.category] = item.obsrValue;
-            });
+  const today = new Date();
+  const todayStr = formatDate(today);
+  console.log(today);
+  console.log(todayStr);
 
-            const div = document.createElement('div');
-            div.className = 'weather-card';
-            console.log(parsedData);
-            div.innerHTML = `
-                <div class="card location"><span class="icons icon-location-pin"></span>안산</div>
-                <div class="card t1h">
-                    <div><span class="icon-temp"></span>temperature</div>
-                    <div class="temp-value">${parsedData[T1H]}</div>
-                </div>
-                <div class="card reh">humidity${parsedData[REH]}</div>
-                <div class="card rn1">precipitation${parsedData[RN1]}</div>
-                <div class="card wsd">wind speed${parsedData[WSD]}</div>
-                `;
-            weatherNow.appendChild(div);
-        });
+  const hour = today.getHours();
+  let baseDate;
+  if (hour >= 5) {
+    baseDate = todayStr;
+  } else {
+    const yesterday = new Date(today);
+    console.log(yesterday);
+    yesterday.setDate(today.getDate() - 1);
+    baseDate = formatDate(yesterday);
+  }
+
+  // 데이터 오늘 오전 5시 기준
+  // 시간도 동적으로 할당해주면 좋을듯
+  fetch(
+    `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=9424b102fa31a2cdf077b43d93ffbbd26e040beb866f241236f5a5e7913ab3ea&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${baseDate}&base_time=0500&nx=57&ny=121`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const datas = data.response.body.items.item;
+      // datas.forEach((item) => {
+      //   console.log('fcstTime:', item.fcstTime, typeof item.fcstTime);
+      // });
+
+      const filtered = datas.filter(
+        (item) => item.fcstDate === todayStr && item.fcstTime === '0600'
+      );
+      console.log(filtered);
+      console.log(baseDate);
+
+      const parsedData = {};
+      filtered.forEach((item) => {
+        parsedData[item.category] = item.fcstValue;
+      });
+      console.log(parsedData);
+
+      // SKY(하늘상태)에 따라 이미지변경 추가해야됨 (3가지) 091825
+      const div = document.createElement('div');
+      div.className = 'weather-card';
+      div.innerHTML = `
+        <div class="card">
+          <div class="card-grid">
+            <div class="card-title">Location</div>
+            <div class="card-value-small">Jungang</div>
+          </div>
+          <div class="card-grid">
+            <div class="card-title">Today</div>
+            <div class="card-value-small">${todayStr}</div>
+          </div>
+          <div class="card-grid">
+            <div class="card-title">Current Weather</div>
+            <img src="/src/assets/images/wind.svg" alt="wind" />
+          </div>
+          <div class="card-grid">
+            <div class="card-title">Temperature</div>
+            <div class="card-value-main">${parsedData.TMP}</div>
+          </div>
+          <div class="card-grid">
+            <div>
+              <div class="card-title">Precipitation</div>
+              <img src="/src/assets/images/cape.svg" alt="cape" />
+            </div>
+            <div class="card-value-sub">${parsedData.POP}%</div>
+          </div>
+          <div class="card-grid">
+            <div class="card-title">Humidity</div>
+            <div class="card-value-sub">${parsedData.REH}</div>
+          </div>
+          <div class="card-grid">
+            <div class="card-title">Wind speed</div>
+            <div class="card-value-sub">${parsedData.WSD}</div>
+          </div>
+        </div>
+      `;
+      const weatherNow = document.querySelector('.weather-now');
+      weatherNow.appendChild(div);
+    });
 } catch (e) {
-    console.error(e);
+  console.error(e);
 }
