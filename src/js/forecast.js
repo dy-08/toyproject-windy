915e52fa676267b0b59c85775b825581a6054344
Youngho Kwon <dy-08@naver.com>
2025-09-22 15:51:32 +0900

    Refactor: 페이지별 JS 정적 삽입 방식을 connectScript 함수로 동적 로딩 처리

--- src/js/forecast.js
+++ src/js/forecast.js
@@ -1,36 +1,35 @@
 try {
-  // dateKey 할당
-  const now = new Date();
-  const hour = now.getHours();
-  let dateKey;
-  if (hour >= 5) {
-    dateKey = `${now.getFullYear()}${String(now.getMonth() + 1).padStart(
-      2,
-      '0'
-    )}${String(now.getDate()).padStart(2, '0')}`;
-  } else {
-    dateKey = `${now.getFullYear()}${String(now.getMonth() + 1).padStart(
-      2,
-      '0'
-    )}${String(now.getDate() - 1).padStart(2, '0')}`;
-  }
-  console.log(dateKey);
+    // dateKey 할당
+    const now = new Date();
+    const hour = now.getHours();
+    let dateKey;
+    if (hour >= 5) {
+        dateKey = `${now.getFullYear()}${String(now.getMonth() + 1).padStart(2, '0')}${String(now.getDate()).padStart(
+            2,
+            '0'
+        )}`;
+    } else {
+        dateKey = `${now.getFullYear()}${String(now.getMonth() + 1).padStart(2, '0')}${String(
+            now.getDate() - 1
+        ).padStart(2, '0')}`;
+    }
+    console.log(dateKey);
 
-  fetch(
-    `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=9424b102fa31a2cdf077b43d93ffbbd26e040beb866f241236f5a5e7913ab3ea&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${dateKey}&base_time=0500&nx=57&ny=121`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      const res = data.response.body.items.item;
-      /**
-       * Pick: 오늘시간, TMP(1시간기온), SKY(하늘상태)
-       **/
-      const hourlyWeatherToday = [];
+    fetch(
+        `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=9424b102fa31a2cdf077b43d93ffbbd26e040beb866f241236f5a5e7913ab3ea&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${dateKey}&base_time=0500&nx=57&ny=121`
+    )
+        .then((res) => res.json())
+        .then((data) => {
+            const res = data.response.body.items.item;
+            /**
+             * Pick: 오늘시간, TMP(1시간기온), SKY(하늘상태)
+             **/
+            const hourlyWeatherToday = [];
 
-      res.forEach((item) => {
-        console.log(item);
-      });
-    });
+            res.forEach((item) => {
+                console.log(item);
+            });
+        });
 } catch (e) {
-  console.error(e);
+    console.error(e);
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,31 @@
 let content = document.getElementById('content');
 let link = document.querySelectorAll('#pages a');
-link.forEach((item) => {
-  item.addEventListener('click', () => {
-    let pagePath = `./src/html/${item.getAttribute('data-page')}`;
-    let pageName = item.getAttribute('data-page'); // weather-now.html
-    fetch(pagePath)
-      .then((res) => res.text())
-      .then((data) => {
-        content.innerHTML = data;
 
-        // 기존 동적 스크립트 제거
-        document
-          .querySelectorAll('script[data-dynamic]')
-          .forEach((s) => s.remove());
-        // 페이지별 JS 실행
-        if (pageName === 'weather-now.html') {
-          const script = document.createElement('script');
-          script.src = './src/js/weather-now.js';
-          script.setAttribute('data-dynamic', 'true');
-          document.body.appendChild(script);
-        }
-      })
-      .catch((e) => console.log(e));
-  });
+// 페이지별 JS 실행
+function connectScript(a) {
+    const script = document.createElement('script');
+    const page = a.slice(0, -5);
+    script.src = `./src/js/${page}.js`;
+    script.setAttribute('data-dynamic', 'true');
+    document.body.appendChild(script);
+}
+
+link.forEach((item) => {
+    item.addEventListener('click', () => {
+        let pagePath = `./src/html/${item.getAttribute('data-page')}`;
+        let pageName = item.getAttribute('data-page'); // weather-now.html
+        console.log(pageName);
+
+        fetch(pagePath)
+            .then((res) => res.text())
+            .then((data) => {
+                content.innerHTML = data;
+
+                // 기존 동적 스크립트 제거
+                document.querySelectorAll('script[data-dynamic]').forEach((s) => s.remove());
+
+                connectScript(pageName);
+            })
+            .catch((e) => console.log(e));
+    });
 });
