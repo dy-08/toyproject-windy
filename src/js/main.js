99ae84dd2e5da3663913c3c9cdd47e299646bc03
Youngho Kwon <dy-08@naver.com>
2025-09-23 17:47:17 +0900

    .

--- src/js/main.js
+++ src/js/main.js
@@ -3,31 +3,29 @@ let link = document.querySelectorAll('#pages a');
 
 // 페이지별 JS 실행
 function connectScript(a) {
-  const script = document.createElement('script');
-  const page = a.slice(0, -5);
-  script.src = `./src/js/${page}.js`;
-  script.setAttribute('data-dynamic', 'true');
-  document.body.appendChild(script);
+    const script = document.createElement('script');
+    const page = a.slice(0, -5);
+    script.src = `./src/js/${page}.js`;
+    script.setAttribute('data-dynamic', 'true');
+    document.body.appendChild(script);
 }
 
 link.forEach((item) => {
-  item.addEventListener('click', () => {
-    let pagePath = `./src/html/${item.getAttribute('data-page')}`;
-    let pageName = item.getAttribute('data-page'); // weather-now.html
+    item.addEventListener('click', () => {
+        let pagePath = `./src/html/${item.getAttribute('data-page')}`;
+        let pageName = item.getAttribute('data-page'); // weather-now.html
 
-    fetch(pagePath)
-      .then((res) => res.text())
-      .then((data) => {
-        content.innerHTML = data;
+        fetch(pagePath)
+            .then((res) => res.text())
+            .then((data) => {
+                content.innerHTML = data;
 
-        // 기존 동적 스크립트 제거
-        document
-          .querySelectorAll('.script[data-dynamic]')
-          .forEach((s) => s.remove());
-        connectScript(pageName);
-      })
-      .catch((e) => console.log(e));
-  });
+                // 기존 동적 스크립트 제거
+                document.querySelectorAll('.script[data-dynamic]').forEach((s) => s.remove());
+                connectScript(pageName);
+            })
+            .catch((e) => console.log(e));
+    });
 });
 
 // 모바일 메뉴 이벤트
@@ -38,17 +36,21 @@ const m_logo = document.getElementById('mobile-bg-logo');
 const body = document.querySelector('body');
 let isClick = false;
 m_menu.addEventListener('click', () => {
-  if (!isClick) {
-    m_bg.style.width = '80%';
-    pages.style.left = '2.4rem';
-    m_logo.style.opacity = 1;
-    body.style.overflow = 'hidden';
-    isClick = true;
-  } else {
-    m_bg.style.width = '0';
-    pages.style.left = '-100%';
-    m_logo.style.opacity = 0;
-    body.style.overflow = 'visible';
-    isClick = false;
-  }
+    if (!isClick) {
+        m_bg.style.width = '80%';
+        pages.style.left = '2.4rem';
+        m_logo.style.opacity = 1;
+        body.style.overflow = 'hidden';
+        isClick = true;
+    } else {
+        m_bg.style.width = '0';
+        pages.style.left = '-100%';
+        m_logo.style.opacity = 0;
+        body.style.overflow = 'visible';
+        isClick = false;
+    }
 });
+
+// 캐릭터 스크롤 이벤트
+let wsy = window.scrollY;
+console.log(wsy);
---
+++ src/js/promotions.js
@@ -0,0 +1,11 @@
+let startValue = 1;
+while (startValue <= 15) {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    img.src = `./src/assets/images/webtoon_${String(startValue).padStart(2, 0)}.jpg`;
+    img.alt = '웹툰 광고이미지';
+    img.className = 'imgs';
+    li.appendChild(img);
+    document.querySelector('.ads').appendChild(li);
+    startValue++;
+}
