35ee43fe5a38c8bbe9ae8cbe3789c0dfefacc5e0
Youngho Kwon <dy-08@naver.com>
2025-09-29 17:46:06 +0900

    fix: 광고 노출에 대한 안내 문구 추가 (사용자 피드백 반영)

--- src/js/promotions.js
+++ src/js/promotions.js
@@ -4,21 +4,19 @@
  */
 
 export async function ads(n) {
-  const ul = document.querySelector('.ads');
-  let startValue = 1;
-  while (startValue <= n) {
-    const li = document.createElement('li');
-    const img = document.createElement('img');
-    img.src = `./src/assets/images/webtoon_${String(startValue).padStart(
-      2,
-      0
-    )}.jpg`;
-    img.alt = '웹툰 광고이미지';
-    img.className = 'imgs';
-    li.appendChild(img);
-    document.querySelector('.ads').appendChild(li);
-    startValue++;
-  }
-  ul.innerHTML += ul.innerHTML;
+    const ul = document.querySelector('.ads');
+    let startValue = 1;
+    while (startValue <= n) {
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+        img.src = `./src/assets/images/webtoon_${String(startValue).padStart(2, 0)}.jpg`;
+        img.alt = '웹툰 광고이미지';
+        img.className = 'imgs';
+        li.appendChild(img);
+        document.querySelector('.ads').appendChild(li);
+        startValue++;
+    }
+    ul.innerHTML += ul.innerHTML;
 }
+
 ads(15);
